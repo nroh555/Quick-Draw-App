b354c464aca2729066606b9efee7c3cf4e6749d0
lukisoo <84960461+lukisoo@users.noreply.github.com>
2022-10-10 02:11:29 +1300

    feat - added badge desciption text to update on badge click

--- src/main/java/nz/ac/auckland/se206/ProfileController.java
+++ src/main/java/nz/ac/auckland/se206/ProfileController.java
@@ -52,6 +52,9 @@ public class ProfileController {
 
   @FXML private Button b12b;
 
+  // Create hashmap to store details about the badges
+  private HashMap<Integer, String> badgesHashMap = new HashMap<Integer, String>();
+
   // Create hashmap to store all of the users.
   private HashMap<String, User> usersHashMap = new HashMap<String, User>();
 
@@ -80,6 +83,24 @@ public class ProfileController {
     MenuController menuController = menuLoader.getController();
     this.currentUser = menuController.getCurrentUser();
     this.usersHashMap = menuController.getUsersHashMap();
+
+    // Check if badges hashmap has been populated yet
+    if (badgesHashMap.isEmpty()) {
+      // If empty, populate with badge details
+      badgesHashMap.put(1, "Win in under 5 seconds.");
+      badgesHashMap.put(2, "Win in under 10 seconds.");
+      badgesHashMap.put(3, "Win in under 30 seconds.");
+      badgesHashMap.put(4, "Win 50 games.");
+      badgesHashMap.put(5, "Get 5 consecutive wins.");
+      badgesHashMap.put(6, "Get 2 consecutive wins.");
+      badgesHashMap.put(7, "Lose 50 games.");
+      badgesHashMap.put(8, "Get 5 consecutive losses.");
+      badgesHashMap.put(9, "Get 2 consecutive losses.");
+      badgesHashMap.put(10, "Win with all medium difficulty.");
+      badgesHashMap.put(11, "Win with all hard difficulty.");
+      badgesHashMap.put(12, "Win with all max difficulty.");
+      System.out.println("empty");
+    }
 
     // Update welcome label
     welcomeLabel.setText("Welcome, " + currentUser.getUsername() + "!");
@@ -125,49 +146,73 @@ public class ProfileController {
 
   /** When badge #1 is clicked */
   @FXML
-  private void onB1Click() {}
+  private void onB1Click() {
+    badgeDescription.setText(badgesHashMap.get(1));
+  }
 
   /** When badge #2 is clicked */
   @FXML
-  private void onB2Click() {}
+  private void onB2Click() {
+    badgeDescription.setText(badgesHashMap.get(2));
+  }
 
   /** When badge #3 is clicked */
   @FXML
-  private void onB3Click() {}
+  private void onB3Click() {
+    badgeDescription.setText(badgesHashMap.get(3));
+  }
 
   /** When badge #4 is clicked */
   @FXML
-  private void onB4Click() {}
+  private void onB4Click() {
+    badgeDescription.setText(badgesHashMap.get(4));
+  }
 
   /** When badge #5 is clicked */
   @FXML
-  private void onB5Click() {}
+  private void onB5Click() {
+    badgeDescription.setText(badgesHashMap.get(5));
+  }
 
   /** When badge #6 is clicked */
   @FXML
-  private void onB6Click() {}
+  private void onB6Click() {
+    badgeDescription.setText(badgesHashMap.get(6));
+  }
 
   /** When badge #7 is clicked */
   @FXML
-  private void onB7Click() {}
+  private void onB7Click() {
+    badgeDescription.setText(badgesHashMap.get(7));
+  }
 
   /** When badge #8 is clicked */
   @FXML
-  private void onB8Click() {}
+  private void onB8Click() {
+    badgeDescription.setText(badgesHashMap.get(8));
+  }
 
   /** When badge #9 is clicked */
   @FXML
-  private void onB9Click() {}
+  private void onB9Click() {
+    badgeDescription.setText(badgesHashMap.get(9));
+  }
 
   /** When badge #10 is clicked */
   @FXML
-  private void onB10Click() {}
+  private void onB10Click() {
+    badgeDescription.setText(badgesHashMap.get(10));
+  }
 
   /** When badge #11 is clicked */
   @FXML
-  private void onB11Click() {}
+  private void onB11Click() {
+    badgeDescription.setText(badgesHashMap.get(11));
+  }
 
   /** When badge #12 is clicked */
   @FXML
-  private void onB12Click() {}
+  private void onB12Click() {
+    badgeDescription.setText(badgesHashMap.get(12));
+  }
 }
