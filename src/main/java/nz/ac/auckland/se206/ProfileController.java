962096a26dcb60c29a75e9e65b839d7c7e2937d2
lukisoo <84960461+lukisoo@users.noreply.github.com>
2022-10-10 01:46:58 +1300

    feat - added functionality for the buttons to be able to be clicked

--- src/main/java/nz/ac/auckland/se206/ProfileController.java
+++ src/main/java/nz/ac/auckland/se206/ProfileController.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.profile.User;
 
@@ -21,9 +22,35 @@ public class ProfileController {
 
   @FXML private TextArea usedWordsBox;
 
+  @FXML private Text badgeDescription;
+
   @FXML private Button backButton;
 
   @FXML private Button logoutButton;
+
+  @FXML private Button b1b;
+
+  @FXML private Button b2b;
+
+  @FXML private Button b3b;
+
+  @FXML private Button b4b;
+
+  @FXML private Button b5b;
+
+  @FXML private Button b6b;
+
+  @FXML private Button b7b;
+
+  @FXML private Button b8b;
+
+  @FXML private Button b9b;
+
+  @FXML private Button b10b;
+
+  @FXML private Button b11b;
+
+  @FXML private Button b12b;
 
   // Create hashmap to store all of the users.
   private HashMap<String, User> usersHashMap = new HashMap<String, User>();
@@ -95,4 +122,52 @@ public class ProfileController {
     Scene sceneThatThisButtonIsIn = btnThatWasClicked.getScene();
     sceneThatThisButtonIsIn.setRoot(SceneManager.getUi(AppUi.DASHBOARD));
   }
+
+  /** When badge #1 is clicked */
+  @FXML
+  private void onB1Click() {}
+
+  /** When badge #2 is clicked */
+  @FXML
+  private void onB2Click() {}
+
+  /** When badge #3 is clicked */
+  @FXML
+  private void onB3Click() {}
+
+  /** When badge #4 is clicked */
+  @FXML
+  private void onB4Click() {}
+
+  /** When badge #5 is clicked */
+  @FXML
+  private void onB5Click() {}
+
+  /** When badge #6 is clicked */
+  @FXML
+  private void onB6Click() {}
+
+  /** When badge #7 is clicked */
+  @FXML
+  private void onB7Click() {}
+
+  /** When badge #8 is clicked */
+  @FXML
+  private void onB8Click() {}
+
+  /** When badge #9 is clicked */
+  @FXML
+  private void onB9Click() {}
+
+  /** When badge #10 is clicked */
+  @FXML
+  private void onB10Click() {}
+
+  /** When badge #11 is clicked */
+  @FXML
+  private void onB11Click() {}
+
+  /** When badge #12 is clicked */
+  @FXML
+  private void onB12Click() {}
 }
